acd6bc6345f7febc93f89016fcd5db8a276912fb
Syuhei Nara <70096064+besumero628@users.noreply.github.com>
2022-02-21 17:58:12 +0900

    10-6 オプショナルチェイニング

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Text } from "./Text";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { User } from "./types/user";
+import { UserProfile } from "./UserProfile";
+
+const user: User = {
+  name: "nara",
+  hobbies: ["映画", "ゲーム"]
+};
 
 export default function App() {
   const [todos, setToDos] = useState<Array<TodoType>>([]);
@@ -16,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
